b2012e1aeb89015dc7265d33b707d93ef44e992a
ShadowConYT <iamajay6122@gmail.com>
2023-05-01 01:14:13 +0530

    I Just wanna sleep

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
 import Navbar from "./navbar.component";
+import { Box, Stack, Typography } from '@mui/material';
+import mike from './mike.png';
+
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div>
-          <p>Welcome to Workout Tracker App!</p>
-        </div>
+        <Box sx={{ mt: { lg: '15px', xs: '70px' }, ml: { lg:'70px', sm: '50px' } }} position="relative" p="20px">
+        <Typography color="#006dd6" fontWeight="600" fontSize="26px">Muscle Monitor</Typography>
+        <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+        Sweat and Wet <br />
+        And Repeat
+        </Typography>
+        <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+        Check out the most effective exercises personalized to you
+        </Typography>
+        <Stack>
+        <a className='button' href="#exercises" style={{ marginTop: '30px', textDecoration: 'none', width: '200px', textAlign: 'center', padding: '14px', fontSize: '22px', textTransform: 'none'}}>
+          <div class="button__line"></div>
+          <div class="button__line"></div>
+          <span class="button__text">Explore Exercises</span>
+          <div class="button__drow1"></div>
+          <div class="button__drow2"></div></a>
+        </Stack>
+        <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '155px' }}>
+        Exercise
+        </Typography>
+        <img src={mike} style={{
+          position:'absolute',
+          right:'0',
+          marginTop:'-0.78%',
+          top:'0',
+          padding:'0',
+          width: '45%',
+        }} />
+        </Box>
       </div>
     );
   }
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,53 +1,54 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from './logo.png';
 
 export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-          <Link to="/" className="navbar-brand">
-            Workout Tracker
+        <nav className="navbar bg-* navbar-expand-lg">
+          <Link to="/" id="navb" className="navbar-brand" style={{paddingLeft:'85px'}}>
+            <img src={logo} style={{width:'50px',height:'50px'}} alt="logo" />
           </Link>
           <div className="collpase navbar-collapse">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
-                <Link to="/" className="nav-link">
+                <Link to="/" id="navb" className="nav-link">
                   Home
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/workout" className="nav-link">
+                <Link to="/workout" id="navb" className="nav-link">
                   All Workouts
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/workout/add" className="nav-link">
+                <Link to="/workout/add" id="navb" className="nav-link">
                   Log Workout
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/routine/add" className="nav-link">
+                <Link to="/routine/add" id="navb" className="nav-link">
                   Create Routine
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/exercise" className="nav-link">
+                <Link to="/exercise" id="navb" className="nav-link">
                   All Exercises
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/progression/add" className="nav-link">
+                <Link to="/progression/add" id="navb" className="nav-link">
                   Create Progression
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/exercise/add" className="nav-link">
+                <Link to="/exercise/add" id="navb" className="nav-link">
                   Create Exercise
                 </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/user/add" className="nav-link">
+                <Link to="/user/add" id="navb" className="nav-link">
                   Create User
                 </Link>
               </li>

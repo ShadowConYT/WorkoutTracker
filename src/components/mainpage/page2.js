cab0d4ee62e3f306ab0471ca3a5975aedfbe1e61
ShadowConYT <iamajay6122@gmail.com>
2023-05-12 15:24:15 +0530

    I did something Don't Know What

--- src/components/mainpage/page2.js
+++ src/components/mainpage/page2.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import {Box, Stack, Typography} from '@mui/material';
-import gokuff from './gokuff.gif';
-import { Container } from 'react-bootstrap';
+import goku from './goku.png';
+import {Image, Container } from 'react-bootstrap';
 
 const Slide2 = () => {
   return (
     <Container fluid >
+    <Box sx={{ mt: { lg: '15px', xs: '70px' }, ml: { lg:'70px', sm: '50px' } }} position="relative" p="20px" >
     <Stack>
         <Typography sx={{mt:{lg:'20px',xs:'10px'}}} color={'#27f3ee'} style={{textAlign:'center',letterSpacing:'10px'}} fontSize={{lg:'50px',xs:'30px'}} fontWeight={'900'}>
             WHAT IS MUSCLE MONITOR ?
         </Typography>
         <Stack direction={'row-reverse'}>
-            <Typography sx={{ mt: { lg: '15px', xs: '30px' }, mr: { lg:'0px', sm: '200px' }, fontSize:{lg: '20px',sm:'5px'},margin:{sm:'none'}}}
-             color={'white'} fontFamily={'monospace'} fontStyle={'bold'} fontSize={'20px'} margin={'50px 50px 20px 50px'}>
-            Hear me, Subjects of Ymir. My name is Eren Yeager. I'm adressing my fellow Subjects of Ymir, speaking to you directly 
-            through the power of the Founder. All the walls on the island of Paradis have crumbled to the ground, and the legions 
-            of Titans burried with in have begun their march. My only goal is to protect the lives of the people of Paradis the 
-            island where I was born. Right now, the nations of the world are united in the desire to exterminate my people, and 
-            it won't end with our island. They won't be satsified until every last Subject of Ymir is dead. I won't let them have
-            their way. The Titans of the walls, will continue their march until every trace of life beyond our shores is trampled 
-            flat, and the people of Paradis are all that remains of humanity.
+            <Typography sx={{ mt: { lg: '70px', xs: '30px' },ml:{lg:'100px',sm:'20px'}, mr: { lg:'5%', sm: '20px' },textAlign:'justify', fontSize:{lg: '30px',md:'25px',sm:'20px',xs:'15px'}}}
+             color={'white'} fontFamily={"sans-serif"} fontStyle={'bold'} margin={'50px 50px 20px 50px'}>
+            Muscle Monitor, the cutting-edge web application designed to be your ultimate workout companion. Whether you're a seasoned gym enthusiast or just starting out on your fitness adventure, Muscle Monitor is here to help you achieve your goals, track your progress, and stay motivated every step of the way.
+At Muscle Monitor, we understand the importance of personalized fitness experiences. That's why our web app is meticulously crafted to cater to your unique needs and aspirations. Our mission is to empower you with the tools and knowledge to optimize your workouts, reach new milestones, and transform your body.
             </Typography>
-            <img style={{transform:'scaleX(-1)',      
-          zIndex:-1,display:'none'}} src={gokuff} height={'600px'} width={'600px'}/>
+            <img alt='image' className='img-fluid' style={{transform:'scaleX(-1)',     
+          zIndex:-1,position:'relative',top:'20%',padding:'0',marginLeft:'2%',width:'20%',height:'auto'}} src={goku}/>
         </Stack>
+        
     </Stack>
+    </Box>
     </Container>
   )
 }
